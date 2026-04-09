7b262a7f588ad15c5c6dbfb091b700e86258aa2b
Krystian Tarnowski <kryszczyn.contact@gmail.com>
2026-04-09 13:05:28 +0200

    fixes

--- html, css, js/script.js
+++ html, css, js/script.js
@@ -1,10 +1,15 @@
 let rownanie = "";
+let lastOperation = null;
+let currentOperation = null;
+let lastInput = "";
+let currentInput = "";
 let wynik = "";
 let valueBox = document.getElementById("value-box")
 
 function addnumber(numberToAdd) {
     if (numberToAdd != 0) {
         rownanie += numberToAdd
+        lastInput += numberToAdd
     }
     else if (rownanie.length > 0) {
         rownanie += numberToAdd
@@ -14,14 +19,43 @@ function addnumber(numberToAdd) {
 }
 
 function operation(type) {
-    if ("+-/*".includes(type)) {
-        if (rownanie.length > 0) {
-            let lastChar = rownanie.slice(-1);
+    // if (lastOperation == null) {
+    //     if ("+-/*".includes(type)) {
+    //         if (rownanie.length > 0) {
+    //             let lastChar = rownanie.slice(-1);
+    //             if (!"+-/*".includes(lastChar)) {
+    //                 rownanie += type;
+    //                 valueBox.textContent += type
+    //             }
+    //         }
+    //     }
+    // } else {
 
-            if (!"+-/*".includes(lastChar)) {
-                rownanie += type;
-                valueBox.textContent += type
+    // }
+
+    if (lastOperation == "") {
+        if (("+-/*").includes(type)) {
+            if (rownanie.length > 0 ) {
+                let lastChar = rownanie.slice(-1);
+                if (!"+-/*".includes(lastChar)) {
+                    rownanie += type;
+                    valueBox.textContent += type
+                    lastOperation = type
+                }
             }
         }
+    } else {
+        calculate()
     }
+}
+
+function calculate() {
+    wynik = lastInput + currentInput
+    
+}
+
+function backOne() {
+    let newValue = valueBox.textContent.slice(0, -1);
+    valueBox.textContent = newValue
+    rownanie = newValue
 }
---
+++ html, css, js/script2.js
@@ -0,0 +1,81 @@
+let valueBox = document.getElementById("value-box");
+let lastOperation = ""
+let currentOperation = ""
+let lastInput = ""
+let currentInput = ""
+
+let boxValue = ""
+
+function addnumber(numberToAdd) {
+    if (numberToAdd != 0) {
+        boxValue += numberToAdd
+        currentInput += numberToAdd
+    }
+    else if (boxValue.length > 0) {
+        boxValue += numberToAdd
+        currentInput += numberToAdd
+    }
+    valueBox.textContent = `${lastInput}${currentOperation}${currentInput}`
+    console.log(valueBox)
+}
+
+function operation(operationType) {
+    if (currentInput === "") return;
+    if (lastInput !== "") {
+        calculate(); 
+    }
+    currentOperation = operationType;
+    lastInput = currentInput;
+    currentInput = "";
+    valueBox.textContent = `${lastInput}${currentOperation}`;
+}
+
+function calculate() {
+    if (lastInput === '' || currentInput === '') return;
+    let result = "";
+    let first = parseFloat(lastInput);
+    let scnd = parseFloat(currentInput);
+
+    switch (currentOperation) {
+        case "+":
+            result = first + scnd;
+            break;
+        case "-":
+            result = first - scnd;
+            break;
+        case "*":
+            result = first * scnd;
+            break;
+        case "/":
+            if (scnd === 0) {
+                alert("Nie można dzielić przez 0!");
+                return;
+            }
+            result = first / scnd;
+            break;
+        default:
+    }
+
+    currentInput = result.toString();
+    currentOperation = "";
+    lastInput = "";
+    valueBox.textContent = `${currentInput}`
+}
+
+
+function backOne() {
+    let lastElement = parseInt(valueBox.textContent.slice(-1))
+    console.log(typeof lastElement)
+    if (typeof lastElement === "number") {
+        currentInput = currentInput.slice(0, -1)
+        console.log(currentInput)
+    } else if (("+-/*").includes(type)) {
+        currentOperation = "backed"
+    }
+    valueBox.textContent = valueBox.textContent.slice(0, -1);
+    console.log(lastElement)
+}
+
+function equals() {
+    calculate()
+}
