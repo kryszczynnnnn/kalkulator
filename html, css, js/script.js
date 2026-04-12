7f914d83b9ecfaf3b47772fce8e834778f5e3bf3
Krystian <krystian.tarnowski07@gmail.com>
2026-04-12 23:12:43 +0200

    new draft

--- html, css, js/script.js
+++ html, css, js/script.js
@@ -1,61 +1,108 @@
-let rownanie = "";
-let lastOperation = null;
-let currentOperation = null;
-let lastInput = "";
-let currentInput = "";
-let wynik = "";
-let valueBox = document.getElementById("value-box")
+let valueBox = document.getElementById("value-box");
+let lastInputBox = document.getElementById("previous-input");
+let lastOperation = ""
+let currentOperation = ""
+let lastInput = ""
+let currentInput = ""
+
+let boxValue = ""
 
 function addnumber(numberToAdd) {
     if (numberToAdd != 0) {
-        rownanie += numberToAdd
-        lastInput += numberToAdd
+        boxValue += numberToAdd
+        currentInput += numberToAdd
     }
-    else if (rownanie.length > 0) {
-        rownanie += numberToAdd
+    else if (boxValue.length > 0) {
+        boxValue += numberToAdd
+        currentInput += numberToAdd
     }
-    valueBox.textContent = rownanie
-    console.log(rownanie)
+    valueBox.textContent = `${lastInput}${currentOperation}${currentInput}`
+    console.log(valueBox)
 }
 
-function operation(type) {
-    // if (lastOperation == null) {
-    //     if ("+-/*".includes(type)) {
-    //         if (rownanie.length > 0) {
-    //             let lastChar = rownanie.slice(-1);
-    //             if (!"+-/*".includes(lastChar)) {
-    //                 rownanie += type;
-    //                 valueBox.textContent += type
-    //             }
-    //         }
-    //     }
-    // } else {
+function operation(operationType) {
+    if (currentInput === "") return;
+    if (lastInput !== "") {
+        calculate(); 
+    }
+    currentOperation = operationType;
+    lastInput = currentInput;
+    currentInput = "";
+    valueBox.textContent = `${lastInput}${currentOperation}`;
+}
 
-    // }
-
-    if (lastOperation == "") {
-        if (("+-/*").includes(type)) {
-            if (rownanie.length > 0 ) {
-                let lastChar = rownanie.slice(-1);
-                if (!"+-/*".includes(lastChar)) {
-                    rownanie += type;
-                    valueBox.textContent += type
-                    lastOperation = type
-                }
-            }
-        }
-    } else {
-        calculate()
+function instantOperation(type) {
+    let result;
+    switch (type) {
+        case "1/x":
+            result = 1/currentInput;
+            break;
+        case "x2":
+            result = currentInput * currentInput;
+            break;
+        case "sqrt":
+            result = Math.sqrt(currentInput)
+            break;
+        default:
+            return;
+    }
+    if (result) {
+        currentInput = result
+        valueBox.textContent = `${currentInput}`
     }
 }
 
 function calculate() {
-    wynik = lastInput + currentInput
-    
+    if (lastInput === '' || currentInput === '') return;
+    let result = "";
+    let first = parseFloat(lastInput);
+    let scnd = parseFloat(currentInput);
+
+    switch (currentOperation) {
+        case "+":
+            result = first + scnd;
+            break;
+        case "-":
+            result = first - scnd;
+            break;
+        case "*":
+            result = first * scnd;
+            break;
+        case "/":
+            if (scnd === 0) {
+                alert("Nie można dzielić przez 0!");
+                return;
+            }
+            result = first / scnd;
+            break;
+        default:
+    }
+
+    currentInput = result.toString();
+    currentOperation = "";
+    lastInput = "";
+    valueBox.textContent = `${currentInput}`
 }
 
+
 function backOne() {
-    let newValue = valueBox.textContent.slice(0, -1);
-    valueBox.textContent = newValue
-    rownanie = newValue
+    let lastElement = valueBox.textContent.slice(-1)
+    console.log(typeof lastElement)
+    if (!"+-/*".includes(lastElement)) {
+        currentInput = currentInput.slice(0, -1)
+        console.log(currentInput)
+        valueBox.textContent = valueBox.textContent.slice(0, -1);
+    }
+    console.log(lastElement)
+}
+
+function clearDisplay() {
+    currentInput = '';
+    lastInput = '';
+    currentOperation = '';
+    valueBox.textContent = "0";
+}
+
+function equals() {
+    calculate()
 }
